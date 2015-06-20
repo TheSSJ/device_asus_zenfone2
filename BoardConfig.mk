88e27d2154e8fdf51c1c604103b7ef4d72b2b75e
TheSSJ <bla@bla.de>
2015-06-20 06:53:27 -0700

    added 2nd BL and stock recovery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,6 +11,7 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 USE_OPENGL_RENDERER := true
 TARGET_GLOBAL_CFLAGS += -march=atom -mstackrealign -DUSE_SSSE3 -DUSE_SSE2 -mfpmath=sse
 TARGET_PREBUILT_KERNEL := device/asus/zenfone2/zImage
+TARGET_BOOTLOADER_IS_2ND := true 
 
 BOARD_FLASH_BLOCK_SIZE := 2048
 BOARD_KERNEL_CMDLINE := init=/init pci=noearly loglevel=0 vmalloc=256M androidboot.hardware=mofd_v1
@@ -18,7 +19,7 @@ BOARD_KERNEL_CMDLINE += watchdog.watchdog_thresh=60 androidboot.spid=xxxx:xxxx:x
 BOARD_KERNEL_CMDLINE += snd_pcm.maximum_substreams=8 ip=50.0.0.2:50.0.0.1::255.255.255.0::usb0:on debug_locks=0
 
 # TWRP
-TARGET_RECOVERY_INITRC := device/asus/zenfone2/init.rc
+#TARGET_RECOVERY_INITRC := device/asus/zenfone2/init.rc
 TARGET_PREBUILT_RECOVERY_KERNEL := device/asus/zenfone2/zImage
 TARGET_RECOVERY_FSTAB := device/asus/zenfone2/twrp.fstab
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
--- device_zenfone2.mk
+++ device_zenfone2.mk
@@ -1,1 +1,3 @@
-
+PRODUCT_COPY_FILES := device/asus/zenfone2/sbin/adbd:recovery/root/sbin/adbd
+PRODUCT_COPY_FILES += device/asus/zenfone2/sbin/adbd-recovery:recovery/root/sbin/adbd-recovery
+PRODUCT_COPY_FILES += device/asus/zenfone2/init.recovery.mofd_v1.rc:recovery/root/init.recovery.mofd_v1.rc
